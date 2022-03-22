3132f1e7d1cdfec2ee7f07af72a14f425fae9438
Edson Ramos <eds.clara@gmail.com>
2022-03-22 15:59:02 -0300

    alterando cadastrar equipamentos

---
+++ static/JScript/scriptCadastrarEquipamentos.js
@@ -0,0 +1,48 @@
+
+
+function cadastrar_usuario() {    
+    let nome = document.getElementById("nome").value;
+    let sobreNome = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("password").value
+    let cSenha = document.getElementById("confpassword").value;
+    let select = document.getElementById("tipos")
+    let tipo = select.options[select.selectedIndex].value
+
+    let dados_usuarios = {
+       nome : nome,
+       sobreNome : sobreNome,
+       email : email,
+       senha : senha,
+       cSenha : cSenha,
+       tipo : tipo  
+       
+    }
+    if(nome == "" || sobreNome == "" || email == "" || senha == "" || cSenha == "" || tipo == ""){
+        return alert("Todos os Campos São Obrigatorios!")
+    }else{
+       if (senha == cSenha) {
+                fetch("/cadastrar_usuarios",
+            {
+                method: "POST",
+                body:JSON.stringify(dados_usuarios),
+                headers:{
+                    "Content-Type" : "application/json"
+                }
+            })
+            .then((resposta) => {
+                if (resposta.status == 200)
+                    return resposta.text()
+                else
+                    return "Erro Ao Cadastrar Usuário"
+            })
+            .then((repostaTexto) => {
+                alert(repostaTexto)
+                document.location.reload(true);
+            })
+        }else
+            return alert("ERROr: Senhas Diferentes!") 
+            }
+    
+}
+    
--- static/JScript/scriptCadastro.js
+++ static/JScript/scriptCadastro.js
@@ -179,38 +179,5 @@ submit.addEventListener('click', function(e){
 });
 
 
-function cadastrar_equipamento() {
-	let nome = document.getElementById("nome_equipamento").value;
-	let id = document.getElementById("id_equipamento").value;
-	let linha = document.getElementById("linha").value;
-	let local = document.getElementById("local").value;
-    
-	let dados_equipamento = {
-		id: id,
-        nome: nome,		
-		linha: linha,
-		trecho: local		
-    
-	}
-	fetch("/cadastro_maquinas",
-    {
-        method:"POST",
-        body:JSON.stringify(dados_equipamento),
-        headers:{
-            "Content-Type": "application/json"
-        }        
-    })    
-    .then((resposta) => {
-        
-        if (resposta.status == 200)
-            return resposta.text()
-        else 
-            return "Erro Ao Cadastrar o Equipamento"          
-    })
-    .then((respostaTexto) => {
-        alert(respostaTexto)
-    })  
-    
-    
-}
+
 
