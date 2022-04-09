ff141e96b5cc55cb027280a7c7a2a83aad1a4072
Edson Ramos <eds.clara@gmail.com>
2022-04-09 19:10:47 -0300

    teste

--- connection.py
+++ connection.py
@@ -2,24 +2,26 @@
 import mysql.connector
 
 
-
 try:
-	#connection = mysql.connector.connect(host='us-cdbr-east-05.cleardb.net1S', user='b7f10ac575aafb', password='59ccfaa1', database='heroku_36b6e4d18a8ca91')
-	#connection = mysql.connector.connect(host='db4free.net', user='easylub2022', password='easylub2022', database='easylub')
-	connection = mysql.connector.connect(host='sql10.freesqldatabase.com', user='sql10482761', password='qEyF8wJVcN', database='sql10482761')
-	#connection = mysql.connector.connect(host='localhost', user='root', password='', database='easylub')
-	if connection.is_connected():
-		db_info = connection.get_server_info()
-		print("Conectado ao Servidor MySQL Versão ", db_info)
-	cursor = connection.cursor()
+    #connection = mysql.connector.connect(host='us-cdbr-east-05.cleardb.net1S', user='b7f10ac575aafb', password='59ccfaa1', database='heroku_36b6e4d18a8ca91')
+    #connection = mysql.connector.connect(host='db4free.net', user='easylub2022', password='easylub2022', database='easylub')
+    connection = mysql.connector.connect(
+        host='sql10.freesqldatabase.com1', user='sql10482761', password='qEyF8wJVcN', database='sql10482761')
+    #connection = mysql.connector.connect(host='localhost', user='root', password='', database='easylub')
+    if connection.is_connected():
+        db_info = connection.get_server_info()
+        print("Conectado ao Servidor MySQL Versão ", db_info)
+    cursor = connection.cursor()
 except mysql.connector.Error as error:
-	print("Erro ao Conectar Com o Banco de Dados")
+    print("Erro ao Conectar Com o Banco de Dados")
+
 
 def getConnection():
-	return (connection, cursor)
+    return (connection, cursor)
+
 
 def closeConnection():
-	if connection.is_connected():
-		cursor.close()
-		connection.close()
-	print("Conexao Encerrada!")
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+    print("Conexao Encerrada!")
